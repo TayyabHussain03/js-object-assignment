2c2d1bbc04dd57864d06a376b7bf47e2abd7aea8
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-06-30 21:28:52 +0500

    question2 added

---
+++ app.js
@@ -0,0 +1,16 @@
+// **********QUESTION 2**********
+
+// var property = {
+//     name: "Tayyab Hussain",
+//     email: "tayyab@gmail.com",
+//     password: "12345",
+//     age: "17",
+//     gender: "Male",
+//     city: "Karachi",
+//     country: "Pakistan",
+// }
+
+// console.log("age" in property)
+// console.log("country" in property)
+// console.log("firstName" in property)
+// console.log("lastName" in property)
