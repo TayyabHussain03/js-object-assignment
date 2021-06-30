0508ea7d3fa6400a68f2218ff6d4845bf59a3bba
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-06-30 21:34:48 +0500

    question3 added

--- app.js
+++ app.js
@@ -14,3 +14,19 @@
 // console.log("country" in property)
 // console.log("firstName" in property)
 // console.log("lastName" in property)
+
+
+// **********QUESTION 3**********
+
+// function Student(name,id,gender,password){
+//     this.name = name;
+//     this.id = id;
+//     this.gender = gender;
+//     this.password = password;
+// }
+
+// var plan1 = new Student("Tayyab","224","Male","12345")
+// var plan2 = new Student("Ahmed","22445","Male","54321")
+
+// console.log(plan1)
+// console.log(plan2)
